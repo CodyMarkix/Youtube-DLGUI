18e949cd3f5d057c569aef9932b442e7ae0f2777
CodyMarkix <hplr63@gmail.com>
2021-12-20 17:16:17 +0100

    did a bit of clean up

--- src/youtubedlgui.py
+++ src/youtubedlgui.py
@@ -2,28 +2,23 @@
 from __future__ import unicode_literals
 from tkinter import *
 from youtube_dl import *
-import requests
+import subprocess
 import os
-
-# Main window
-window = Tk()
-window.geometry("480x180")
-window.title("Youtube DLGUI")
-window.resizable(0,0)
 
 # Function for downloading the video
 def convert():
+    MainMenu.progressbar.pack()
     try:
         SAVE_PATH = '/'.join(os.getcwd().split('/')[:3]) + '/Desktop' # Path to video folder
 
         # Options for video if MP4 is selected
-        if clicked.get() == "MP4":
+        if MainMenu.clicked.get() == "MP4":
             ydl_opts = {
                 'format': 'bestaudio/best',
                 'outtmpl':SAVE_PATH + '/%(title)s.%(ext)s',
             }
         # Same but if MP3 is selected
-        elif clicked.get() == "MP3":
+        elif MainMenu.clicked.get() == "MP3":
             ydl_opts = {
                 'format': 'bestaudio/best',
                 'postprocessors': [{
@@ -35,7 +30,7 @@ def convert():
             }
         # Downloading the video
         with YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url.get()])
+            ydl.download([MainMenu.url.get()])
         convertiondone()
 
     # (Unproper) Error handling
@@ -76,36 +71,44 @@ def syntaxerrordialogue():
     syntaxtext.pack()
     syntaxok.pack()
 
-# Widgets, idk
-logo = Label(window, text="Youtube DLGUI")
-url = Entry(window)
+# Main window
+class MainMenu():
+    window = Tk()
+    window.geometry("480x180")
+    window.title("Youtube DLGUI")
+    window.resizable(0,0)
 
-clicked = StringVar()
-clicked.set("MP4")
-formatdrop = OptionMenu(window, clicked, "MP4", "MP3")
-downloadbutton = Button(window, text="Convert & Download", command=convert)
+    logo = Label(window, text="Youtube DLGUI")
+    url = Entry(window)
+
+    clicked = StringVar()
+    clicked.set("MP4")
+    formatdrop = OptionMenu(window, clicked, "MP4", "MP3")
+    downloadbutton = Button(window, text="Convert & Download", command=convert)
+    
+    terminaloutput = subprocess.check_output
+    progressbar = Label(window, text=terminaloutput)
+
+    # firstclick and on_entry_click function stolen from StackOverflow: https://stackoverflow.com/questions/30491721/how-to-insert-a-temporary-text-in-a-tkinter-entry-widget
+    firstclick = True
+
+    def on_entry_click(event):
+        """function that gets called whenever entry1 is clicked"""        
+        global firstclick
+
+        if MainMenu.firstclick: # if this is the first time they clicked it
+            firstclick = False
+            MainMenu.url.delete(0, "end") # delete all the text in the entry
 
 # Shoving on screen
+MainMenu.logo.pack()
 
-# firstclick and on_entry_click function stolen from StackOverflow: https://stackoverflow.com/questions/30491721/how-to-insert-a-temporary-text-in-a-tkinter-entry-widget
-firstclick = True
-
-def on_entry_click(event):
-    """function that gets called whenever entry1 is clicked"""        
-    global firstclick
-
-    if firstclick: # if this is the first time they clicked it
-        firstclick = False
-        url.delete(0, "end") # delete all the text in the entry
-
-logo.pack()
-
-url.pack()
-url.insert(0, "URL")
-url.bind('<FocusIn>', on_entry_click)
-formatdrop.pack()
-downloadbutton.pack()
+MainMenu.url.pack()
+MainMenu.url.insert(0, "URL")
+MainMenu.url.bind('<FocusIn>', MainMenu.on_entry_click)
+MainMenu.formatdrop.pack()
+MainMenu.downloadbutton.pack()
 
 
 # Looping
-window.mainloop()
+MainMenu.window.mainloop()
